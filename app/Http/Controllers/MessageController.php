6618c870d2181d2513dba8d83dd46046732a487b
tahara3181 <wants@studiom-web.net>
2023-11-04 15:18:12 +0900

    initial commit

---
+++ app/Http/Controllers/MessageController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+use App\Models\Message;
+
+class MessageController extends Controller
+{
+    public function index(): View
+    {
+        $messages = Message::all();
+        return view('message/index', ['messages' => $messages]);
+    }
+}
